a98d6c02660d6f2c6f4d82d1a7d80c95b4331e78
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-03-23 23:23:25 +0100

    Refactor UserTable component to include sorting functionality

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,38 +1,52 @@
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
+import { sort } from 'fast-sort';
+
 
 interface User {
     id: number;
     name: string;
     email: string;
-  }
+}
+interface Props {
+    sortOrder: string;
 
-const UserTable = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'}
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' }
     );
     const users: User[] = await res.json();
-  return (
-            <table className='table table-bordered
-  '>
-      <thead>
-        <tr>
-          <td>
-            <Link href="/users?sortOrder=name">Name</Link>
-          </td>
-          <td>
-            <Link href="/users?sortOrder=email">Email</Link>
-          </td>
-        </tr>
-      </thead>
-      <tbody>
-      {users.map(user => <tr key={user.id}>
 
-        <th>{user.name}</th>
-        <th>{user.email}</th>
-      </tr>)}
-      </tbody>
-    </table>
-  )
+    const sortedUsers = sort(users).asc(
+        sortOrder === 'email' 
+        ? user => user.email 
+        : user => user.name
+        );
+
+
+    return (
+        <table className='table table-bordered
+  '>
+            <thead>
+                <tr>
+                    <td>
+                        <Link href="/users?sortOrder=name">Name</Link>
+                    </td>
+                    <td>
+                        <Link href="/users?sortOrder=email">Emai</Link>
+                    </td>
+                </tr>
+            </thead>
+            <tbody>
+                {sortedUsers.map(user => <tr key={user.id}>
+
+                    <th>{user.name}</th>
+                    <th>{user.email}</th>
+                </tr>)}
+            </tbody>
+        </table>
+    )
 }
 
 export default UserTable
