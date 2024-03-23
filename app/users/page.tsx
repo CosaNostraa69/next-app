f02388bc10b9c989b44dc98e09927e7e15435cee
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-03-23 22:44:39 +0100

    Refactor UsersPage component to use UserTable component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,13 @@
 import React from 'react'
+import UserTable from './UserTable'
 
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'}
-  );
-  const users: User[] = await res.json();
+
   return (
     <>
-    <h1>Users</h1>
-    <table className='table table-bordered
-  '>
-      <thead>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-        </tr>
-      </thead>
-      <tbody>
-      {users.map(user => <tr key={user.id}>
-
-        <th>{user.name}</th>
-        <th>{user.email}</th>
-      </tr>)}
-      </tbody>
-    </table>
+      <h1>Users</h1>
+      <UserTable />
     </>
 
   )
