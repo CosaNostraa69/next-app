8bcc52e5734bcfe3c0b98d69ea3461603875b3a1
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-03-25 14:42:36 +0100

    Add sign out link to authenticated NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -13,7 +13,11 @@ const NavBar = () => {
         <Link href="/" className='mr-5'>Next.js</Link>
         <Link href="/users" className='mr-5'>Users</Link>
         { status === 'loading' && <div>Loading...</div>}
-        { status === 'authenticated' && <div> {session.user?.name} </div>}
+
+        { status === 'authenticated' && <div>
+           {session.user?.name}
+           <Link href='/api/auth/signout' className='ml-3'>Sign Out</Link>
+            </div>}
         { status === 'unauthenticated' && <Link href="/api/auth/signin" className='mr-5'>Login</Link>}
 
     </div>
